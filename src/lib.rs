a567de1dec02a2a50c425c6fe4f0329b1942c6be
Brady Pierce <bradypierce13@gmail.com>
2021-07-03 09:53:15 -0700

    add more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate derive_builder;
 
+#[macro_use]
+extern crate approx;
+
 macro_rules! max {
     ($x:expr) => ( $x );
     ($x:expr, $($xs:expr),+) => {
@@ -30,20 +33,46 @@ pub mod indicators {
 mod tests {
     use std::error::Error;
 
-    use super::indicators::momentum_indicators::*;
     use super::indicators::overlap_studies::*;
-    use super::indicators::pattern_recognition::*;
 
-    #[test]
-    fn it_works() -> Result<(), Box<dyn Error>> {
-        let data = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+    mod real_input_indicator {
+        use super::*;
 
-        let sma = SmaBuilder::default().time_period(4).build()?;
-        let output = sma.calculate(data.as_slice())?;
-        dbg!(sma);
-        dbg!(output);
+        #[test]
+        fn accepts_vec() -> Result<(), Box<dyn Error>> {
+            let sma = SmaBuilder::default().time_period(4).build()?;
+            let expected = vec![2.5, 3.5];
 
-        panic!();
-        Ok(())
+            let data = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+            let output = sma.calculate(data)?;
+            assert_relative_eq!(output.as_slice(), expected.as_slice());
+
+            Ok(())
+        }
+
+        #[test]
+        fn accepts_slice() -> Result<(), Box<dyn Error>> {
+            let sma = SmaBuilder::default().time_period(4).build()?;
+            let expected = vec![2.5, 3.5];
+
+            let data = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+            let output = sma.calculate(data.as_slice())?;
+            assert_relative_eq!(output.as_slice(), expected.as_slice());
+
+            Ok(())
+        }
+
+        #[test]
+        fn accepts_arr() -> Result<(), Box<dyn Error>> {
+            // Can't be generic over fixed length arrays until const generics are stable
+            Ok(())
+        }
+    }
+
+    mod price_input_indicator {
+        use super::*;
+
+        #[test]
+        fn accepts
     }
 }
